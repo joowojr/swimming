e1a01bca5dd1d0565bb19eda5aa8fbb647f9ff8f
joowojr <joowojr@gmail.com>
2026-08-19 18:05:53 +0900

    chore: fe 설정 추가

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  envPrefix: 'API_',
   server: {
     proxy: {
       '/api': 'http://localhost:8080',

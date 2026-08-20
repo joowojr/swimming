a4f36868cde47570f993f83098663c55c644d689
joowojr <joowojr@gmail.com>
2026-08-20 14:04:05 +0900

    refactor: be

--- backend/src/main/java/com/swimming/backend/user/service/UserService.java
+++ backend/src/main/java/com/swimming/backend/user/service/UserService.java
@@ -5,17 +5,11 @@ import com.swimming.backend.user.dto.UserAuthInfo;
 import com.swimming.backend.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(
-        propagation = Propagation.REQUIRED,
-        readOnly = true
-)
 public class UserService {
 
     private final UserRepository userRepository;

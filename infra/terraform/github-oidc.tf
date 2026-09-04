8e56aabb08dd7b2c9eb96b1b6b77c4bc5747769c
joowojr <joowojr@gmail.com>
2026-09-04 10:03:52 +0900

    fix: github_terraform Role PR로 변경

--- infra/terraform/github-oidc.tf
+++ infra/terraform/github-oidc.tf
@@ -39,7 +39,7 @@ data "aws_iam_policy_document" "github_deploy_assume_role" {
     condition {
       test     = "StringEquals"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_repository}:ref:refs/heads/main"]
+      values   = ["repo:${var.github_repository}:pull_request"]
     }
   }
 }
